c28a38d9a36df39f7c4b12bf54e98052960bd3ce
Zelalem Getahun <zelalemgetahun9374@gmail.com>
2023-09-13 12:21:36 +0300

    implemented todo list logic with TaskItem component

--- src/App.tsx
+++ src/App.tsx
@@ -1,9 +1,121 @@
+import { useEffect, useRef, useState } from "react"
+import TaskItem from "./components/TaskItem"
+
+type taskType = {
+    id: number,
+    name: string,
+    completed: boolean
+}
+
+type filterType = "all" | "completed" | "uncompleted"
+const TASK_KEY = "task_list"
+
 function App() {
+    const [tasks, setTasks] = useState<taskType[]>([])
+
+    const [visibleTasks, setVisibleTasks] = useState<taskType[]>([])
+    const [filter, setFilter] = useState<filterType>("all")
+
+    const newTaskInputRef = useRef<HTMLInputElement>(null)
+
+    useEffect(() => {
+        localStorage.setItem(TASK_KEY, JSON.stringify(tasks));
+    }, [tasks])
+
+    useEffect(() => {
+        switch (filter) {
+            case "all":
+                setVisibleTasks(tasks)
+                return
+            case "completed":
+                setVisibleTasks(tasks.filter(task => task.completed === true))
+                return
+            case "uncompleted":
+                setVisibleTasks(tasks.filter(task => task.completed === false))
+                return
+        }
+    }, [tasks, filter])
+
+    function addTask() {
+        const task = newTaskInputRef.current?.value
+        if (task) {
+            setTasks((tasks) => [...tasks, {
+                id: tasks.length == 0 ? 0 : tasks[tasks.length - 1].id + 1,
+                name: task,
+                completed: false
+            }])
+            newTaskInputRef.current.value = ""
+            newTaskInputRef.current.focus();
+        } else {
+            console.log("New task can't be empty");
+        }
+    }
+
+    function deleteTask(id: number) {
+        setTasks((tasks) => tasks.filter(task => task.id !== id))
+    }
+
+    function updateTask(id: number) {
+        setTasks((tasks) => tasks.map(task => {
+            if (task.id === id) {
+                return { ...task, completed: !task.completed }
+            }
+            return task
+        }))
+    }
 
     return (
-        <h1 className="text-3xl font-bold">
-            Todo task list
-        </h1>
+        <main className="flex items-center justify-center min-h-screen bg-gray-2 00">
+            <div className={`flex flex-col items-center gap-4 w-[600px] lg:w-[760px] h-[90vh] m-auto my-4 p-8 bg-[#fbfcfc] border border-gray-300 rounded-xl shadow-2xl`}>
+                <h1 className="text-3xl font-medium text-sky-900 text-center ">
+                    Todo task list
+                </h1>
+                <div className="flex">
+                    <input
+                        placeholder="Add a new task"
+                        className="w-60 px-2 py-1 border-b-2 border-blue-500 focus:outline-none bg-transparent placeholder-gray-400/70 text-gray-700"
+                        type="text"
+                        onKeyDown={e => { e.key === 'Enter' ? addTask() : null }}
+                        ref={newTaskInputRef}
+                    />
+                    
+                    <button
+                        type="submit"
+                        className="ml-2 px-2 py-1.5  text-white bg-blue-500 border-2 border-blue-500 rounded-lg flex"
+                        onClick={addTask}
+                    >
+                        <svg className="h-6 w-6" width="24" height="24" viewBox="0 0 24 24" strokeWidth="2" stroke="currentColor" fill="none" strokeLinecap="round" strokeLinejoin="round">  <path stroke="none" d="M0 0h24v24H0z" />  <circle cx="12" cy="12" r="9" />  <line x1="9" y1="12" x2="15" y2="12" />  <line x1="12" y1="9" x2="12" y2="15" /></svg>
+                        <span> Add</span>
+                    </button>
+                </div>
+                <div className="flex bg-white border divide-x rounded-lg mt-8">
+                    <button className={`flex items-center px-4 py-2 text-sm font-medium rounded-l-lg transition-colors duration-200 sm:text-base sm:px-6 hover:bg-blue-600 hover:text-white ${filter === "all" ? 'text-white bg-blue-600 outline-none' : 'bg-none text-blue-600'}`} onClick={() => { setFilter("all") }}>
+                        <span>All</span>
+                    </button>
+                    <button className={`flex items-center px-4 py-2 text-sm font-medium transition-colors duration-200 sm:text-base sm:px-6 hover:bg-green-500 hover:text-white ${filter === "completed" ? 'text-white bg-green-500 outline-none' : 'bg-none text-green-600'}`} onClick={() => { setFilter("completed") }}>
+                        <span>Completed</span>
+                    </button>
+                    <button className={`flex items-center px-4 py-2 text-sm font-medium rounded-r-lg transition-colors duration-200 sm:text-base sm:px-6 hover:bg-orange-500 hover:text-white ${filter === "uncompleted" ? 'text-white bg-orange-500 outline-none' : 'bg-none text-orange-500'}`} onClick={() => { setFilter("uncompleted") }}>
+                        <span>Uncompleted</span>
+                    </button>
+                </div>
+               
+                <ul id="task_list" className="flex flex-col self-start gap-2 w-full pr-2 overflow-y-auto overflow-x-hidden">
+                    {
+                        visibleTasks.map((task) =>
+                            <TaskItem
+                                key={task.id}
+                                id={task.id}
+                                name={task.name}
+                                completed={task.completed}
+                                updateTask={updateTask}
+                                deleteTask={deleteTask}
+                            />)
+                    }
+                </ul>
+            </div>
+
+        </main>
     )
 }
 
---
+++ src/components/TaskItem.tsx
@@ -0,0 +1,42 @@
+import { useId } from 'react'
+import deleteIcon from '../assets/delete.png'
+
+type taskItemProps = {
+    id: number,
+    name: string,
+    completed: boolean,
+    updateTask: (id: number) => void,
+    deleteTask: (id: number) => void,
+}
+
+const TaskItem = ({ id, name, completed, updateTask, deleteTask }: taskItemProps) => {
+    const checkboxId = useId();
+
+    return (
+        <li className='flex items-center gap-2 w-full'>
+            <input className="hidden grow-0 shrink-0" type="checkbox" id={checkboxId} checked={completed} onChange={() => updateTask(id)} />
+            <label className="flex items-center grow px-2 py-1.5 rounded cursor-pointer hover:bg-gray-100 border border-" htmlFor={checkboxId}>
+                <span className="flex items-center justify-center w-5 h-5 text-transparent border-2 border-gray-300 rounded-full">
+                    <svg className="w-4 h-4 fill-current" viewBox="0 0 20 20" fill="currentColor">
+                        <path fillRule="evenodd" d="M16.707 5.293a1 1 0 010 1.414l-8 8a1 1 0 01-1.414 0l-4-4a1 1 0 011.414-1.414L8 12.586l7.293-7.293a1 1 0 011.414 0z" clipRule="evenodd" />
+                    </svg>
+                </span>
+                <p className={`ml-2 text-lg grow px-2 rounded-sm ${completed ? 'text-[#9CA3AF] line-through' : 'text-gray-900'}`}>{name}</p>
+            </label>
+            
+            <div className="group relative grow-0 shrink-0 flex flex-col align-bottom">
+                <button
+                    type='button'
+                    onClick={() => deleteTask(id)}
+                    className='text-white border border-red-200 hover:bg-red-200 focus:ring-4 focus:outline-none focus:ring-red-300 
+                        font-medium rounded text-sm p-1 text-center inline-flex items-center mr-2'
+                >
+                    <img className='w-4 h-4' src={deleteIcon} alt="delete task" />
+                </button>
+                <p className="absolute top-8 right-0 scale-0 z-10 overflow-visible rounded bg-gray-800 p-2 text-xs text-white group-hover:scale-100">Delete</p>
+            </div>
+        </li>
+    )
+}
+
+export default TaskItem
